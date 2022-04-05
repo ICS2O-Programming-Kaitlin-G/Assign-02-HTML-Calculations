aeb5ab15414077288bc44f8d688d885a54db0052
Katie <99366154+kaitlingreen@users.noreply.github.com>
2022-04-05 11:43:06 +0000

    wow numbers! my internet wasn't working last night so here are the numbers

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Created by: Katie
+// Created on: March 2022
+// This file contains the JS functions for index.html
+
+function submitClicked () {
+  // This function will calculate the quantity of euros and make it appear when the button is clicked :)
+  
+  // Getting canadian dollars from user number field input and casting integer
+  let canadianDollars = parseInt(document.getElementById("canadian-dollars").value);
+  
+  // doing the calculations and declaring the constant
+  const CONVERSION_RATE = "1.38070102499";
+  let valueInEuros = canadianDollars / CONVERSION_RATE;
+  
+  // Printing the value out onto the screen. 
+  document.getElementById('calculated-euros').innerHTML = "$" + canadianDollars + "CAD converted into EUR (euros) is equal to approximately (because the value is rounded to two decimal places) " + valueInEuros "."
