2c62c9a7f310d53f02533a00cf0b8817623a418e
Katie <99366154+kaitlingreen@users.noreply.github.com>
2022-04-08 09:18:24 +0000

    the words have done the thing and now it is time for me to sleep i totally did not forget that this was due nope not me :)

--- js/script.js
+++ js/script.js
@@ -7,12 +7,26 @@ function calculateEurosClicked() {
   // This function will calculate the quantity of euros and make it appear when the button is clicked :)
   
   // Getting canadian dollars from user number field input and casting integer
-  let canadianDollars = parseInt(document.getElementById("canadian-dollars").value);
+  let canadianDollars = parseInt(document.getElementById("canadian-dollars-for-euro-calculation").value);
   
   // doing the calculations and declaring the constant
-  const CONVERSION_RATE = "1.38070102499";
-  let valueInEuros = canadianDollars / CONVERSION_RATE;
+  const CONVERSION_RATE_EUR = "1.38070102499";
+  let valueInEuros = canadianDollars / CONVERSION_RATE_EUR;
   
   // Printing the value out onto the screen. 
   document.getElementById('calculated-euros').innerHTML = "$" + canadianDollars + "CAD converted into EUR (euros) is equal to approximately (because the value is rounded to two decimal places) " + valueInEuros.toFixed(2) + "."
+}
+
+function calculateKronasClicked() {
+  // This function will calculate the quantity of kronas and make it appear when the button is clicked wow its just like the one above it
+  
+  // Getting canadian dollars from user number field input and casting integer
+  let canadianDollars = parseInt(document.getElementById("canadian-dollars-for-krona-calculation").value);
+  
+  // doing the calculations and declaring the constant
+  const CONVERSION_RATE_SEK = "0.13313098665";
+  let valueInKronas = canadianDollars / CONVERSION_RATE_SEK;
+  
+  // Printing the value out onto the screen. 
+  document.getElementById('calculated-kronas').innerHTML = "$" + canadianDollars + "CAD converted into SEK (Swedish Krona) is equal to approximately (because the value is rounded to two decimal places) " + valueInKronas.toFixed(2) + "."
 }
